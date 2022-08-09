3bff1ad3da0f4da27d902d056787f70a82c9b335
Sage Skaff <skaffsage@gmail.com>
2022-08-09 12:24:21 -0700

    Test: merchant dashboard links to bulk discounts index

---
+++ spec/features/merchants/bulk_discounts/index_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'merchant bulk discounts index' do
+  it 'links from merchant dashboard' do
+    pokemart = Merchant.create!(name: 'PokeMart')
+
+    visit "/merchants/#{pokemart.id}"
+
+    click_link 'View All My Discounts'
+
+    expect(current_path).to eq("/merchants/#{pokemart.id}/bulk_discounts")
+  end
+end
