4c513e7d17a8ecc282bb1da3debaf68e6c4dadef
Sage Skaff <skaffsage@gmail.com>
2022-08-09 21:17:18 -0700

    Test: form to create new bulk discount

--- spec/features/merchants/bulk_discounts/new_spec.rb
+++ spec/features/merchants/bulk_discounts/new_spec.rb
@@ -10,4 +10,24 @@ RSpec.describe 'merchant bulk discounts new' do
 
     expect(current_path).to eq("/merchants/#{pokemart.id}/bulk_discounts/new")
   end
+
+  it 'has form to create new bulk discount' do
+    pokemart = Merchant.create!(name: 'PokeMart')
+
+    visit "/merchants/#{pokemart.id}/bulk_discounts"
+
+    expect(page).to_not have_content('Percentage: 10%')
+    expect(page).to_not have_content('Quantity Threshold: 10')
+
+    visit "/merchants/#{pokemart.id}/bulk_discounts/new"
+
+    fill_in 'Percentage', with: 10
+    fill_in 'Quantity Threshold', with: 10
+
+    click_on 'Submit'
+
+    expect(current_path).to eq("/merchants/#{pokemart.id}/bulk_discounts")
+    expect(page).to have_content('Percentage: 10%')
+    expect(page).to have_content('Quantity Threshold: 10')
+  end
 end
